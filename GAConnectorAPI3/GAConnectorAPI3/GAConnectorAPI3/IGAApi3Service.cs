479efb392a75cc68a4d8c6dd60ac8e9d3c070cdb
oscarramosp@gmail.com <oscarramosp@gmail.com@88682032-daca-cca1-4ec6-c0f422605f2f>
2015-03-07 13:48:24 +0000

    Repo created

---
+++ GAConnectorAPI3/GAConnectorAPI3/GAConnectorAPI3/IGAApi3Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace GAConnectorAPI3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGAApi3Service" in both code and config file together.
+    [ServiceContract]
+    public interface IGAApi3Service
+    {
+        [OperationContract]
+        [WebGet]
+        string ExtractData(string Profile, string Dimensions, string Metrics, string StartDate, string EndDate, string extraCols, string extraData);
+    }
+}
---
+++ GAConnectorAPI3/GAConnectorAPI3/GAConnectorAPI3HostAPP/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace GAConnectorAPI3HostAPP
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost host;
+
+        private WebServiceHost hostX;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //hostX = new WebServiceHost(typeof(GAConnectorAPI3.GAApi3Service), new Uri("http://localhost:8090/"));
+            //ServiceEndpoint ep = host.AddServiceEndpoint(typeof(GAConnectorAPI3.IGAApi3Service), new WebHttpBinding(), "");
+            host = new ServiceHost(typeof(GAConnectorAPI3.GAApi3Service));
+
+            host.Open();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            lblMessage.Text = "Servicio iniciado";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            host = new ServiceHost(typeof(GAConnectorAPI3.GAApi3Service));
+            //hostX = new WebServiceHost(typeof(GAConnectorAPI3.GAApi3Service), new Uri("http://localhost:8090/"));
+            //ServiceEndpoint ep = host.AddServiceEndpoint(typeof(GAConnectorAPI3.IGAApi3Service), new WebHttpBinding(), "");
+            host.Open();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            lblMessage.Text = "Servicio iniciado";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            host.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            lblMessage.Text = "Servicio detenido";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            host.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+
+    }
+}
